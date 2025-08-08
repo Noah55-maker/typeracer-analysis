931b9f1f43ef62adbe858228594ed69ab7c65fe7
Noah <74223879+Noah55-maker@users.noreply.github.com>
2025-08-07 20:10:55 -0700

    tidy up fetch.py

--- fetch.py
+++ fetch.py
@@ -9,7 +9,7 @@ num_races = 3400
 last = True
 sort_type = "last" if last else "fastest"
 
-write_raw = False
+write_raw = True
 
 url = f"https://typeracerdata.com/profile?username={username}&{sort_type}={num_races}"
 response = requests.get(url)
@@ -22,34 +22,33 @@ if response.status_code != 200:
 response_text = f"{response.content}"
 cleanup = response_text.replace("\\r\\n", '\n')
 
+# raw html
+if write_raw:
+	with open(f"raw-{username}.html", 'w') as raw_file:
+		raw_file.write(cleanup)
+
 pattern = re.compile(r"\d+\">\d+\.\d\d")
 matches = pattern.findall(cleanup)
 
-
 date_pattern = re.compile(r"20.*\n.*\d\">\d+.\d\d")
 date_matches = date_pattern.findall(cleanup)
-# print(date_matches)
 
 speeds = []
-# dates = []
 times = []
+
+# parse WPM
 for i in range(len(matches)):
 	speeds.append(matches[i].split('>')[1])
 
+# parse dates
 for i in range(len(date_matches)):
 	td = date_matches[i].split('<')[0]
-	# dates.append(td)
 	times.append(datetime.strptime(td, "%Y-%m-%d %H:%M:%S").timestamp())
 
-# print(dates)
-
+# write data
 with open(f"speeds-{username}.txt", 'w') as race_file:
 	for i in range(len(speeds)):
 		race_file.write(speeds[i])
 		race_file.write(',')
 		race_file.write(str(times[i]))
 		race_file.write('\n')
-
-if write_raw:
-	with open(f"raw-{username}.html", 'w') as raw_file:
-		raw_file.write(cleanup)
